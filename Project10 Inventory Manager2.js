b47f2daffe7d8ca465d842df4a4acc954b18313d
Wojtek Kochan <wkochan1989@gmail.com>
2026-08-10 18:25:37 -0500

    First time working with splice() and findIndex()

--- Project10 Inventory Manager2.js
+++ Project10 Inventory Manager2.js
@@ -1,3 +1,10 @@
+// ========================
+// PROJECT 10 - INVENTORY
+// ========================
+
+
+
+
 const inventory = [];
 
 function addItem(name,value,quantity,rarity,durability) {
@@ -61,7 +68,51 @@ addItem(
 function removeItem(name) {
 
     const itemIndex = inventory.findIndex(item => item.name === name)
-    inventory.splice(itemIndex,1)
 
+    if (itemIndex >= 0) {
+    inventory.splice(itemIndex,1)
+    }
+}
+
+function removeWeapon(name) {
+    const weaponIndex = inventory.findIndex(item => item.name === name)
+
+    if (weaponIndex >= 0){
+        inventory.splice(weaponIndex,1)
+    }
+}
+
+
+function removeBrokenItem(name) {
+    const itemIndex = inventory.findIndex(item => item.name === name)
+
+    if (itemIndex >= 0 && inventory[itemIndex].durability <= 0){
+        inventory.splice(itemIndex,1)
+    }
+}
+
+
+function removeCheapItem(name) {
+    const itemIndex = inventory.findIndex(item => item.name === name)
+
+    if(itemIndex >=0 && inventory[itemIndex].value < 100){
+        inventory.splice(itemIndex,1)
+    }
+}
+
+
+
+// ========================
+// PRACTICE - SIMPLE ARRAYS
+// ========================
+
+const party = ["Gale", "Shadowheart", "Astarion", "Lae'zel"]
+
+function removeCompanion(name) {
+    const partyIndex = party.findIndex(member => member === name)
+
+    if (partyIndex >= 0){
+        party.splice(partyIndex, 1)
+    }
 }
 
